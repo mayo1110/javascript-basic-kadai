7f0159f043ccb4e4c3c7261c9fca50e7dd20c340
mayo1110 <45247716+mayo1110@users.noreply.github.com>
2025-04-08 13:58:40 +0900

    7章課題

---
+++ kadai_007/calc.js
@@ -0,0 +1,25 @@
+// JavaScript Document
+let num=6;
+
+if(num>=1){
+    if(num%3==0){
+        if(num%5==0){
+            console.log('3と5の倍数です');
+        }
+        else{
+            console.log('3の倍数です');
+        }
+    }
+    
+    else if(num%5==0){
+        console.log('5の倍数です');
+    }
+    
+    else{
+        console.log(num);
+    }
+}
+
+else{
+        console.log(num);
+}
