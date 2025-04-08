07563e0a05260df1e9aac2c6600c511dc1370ffa
mayo1110 <45247716+mayo1110@users.noreply.github.com>
2025-04-08 14:13:08 +0900

    修正

--- kadai_007/calc.js
+++ kadai_007/calc.js
@@ -1,14 +1,13 @@
 // JavaScript Document
-let num=6;
+let num=15;
 
 if(num>=1){
-    if(num%3==0){
-        if(num%5==0){
-            console.log('3と5の倍数です');
-        }
-        else{
-            console.log('3の倍数です');
-        }
+	if(num%3==0 && num%5==0){
+		console.log('3と5の倍数です');
+	}
+	
+    else if(num%3==0){
+		console.log('3の倍数です');
     }
     
     else if(num%5==0){
