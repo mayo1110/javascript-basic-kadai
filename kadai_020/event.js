b54e06f0b0b93dcb671fb1989e43b6e2b0cf81b4
mayo1110 <45247716+mayo1110@users.noreply.github.com>
2025-04-10 14:55:05 +0900

    20章課題

---
+++ kadai_020/event.js
@@ -0,0 +1,7 @@
+// JavaScript Document
+const btn=document.getElementById('btn');
+const text=document.getElementById('text');
+
+btn.addEventListener('click',()=>{
+	text.textContent="ボタンをクリックしました";
+})
